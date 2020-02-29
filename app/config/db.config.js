81b9e1475e9e62bb70403fbd8b5eb8d223690cac
Esen <esenkalmanbetov@gmail.com>
2020-02-29 18:56:48 +0600

    modified

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -5,11 +5,11 @@ module.exports = {
   // DB: "testdb",
   // dialect: "mysql",
   
-  // HOST: "ec2-3-230-106-126.compute-1.amazonaws.com",
-  // USER: "qrhhuvveqvggwl",
-  // PASSWORD: "883fb7ebc1603c4e55e172b91fb40a4087f72255a7be7ed1564b176f36e58a24",
-  // DB: "d5hdlrtrpck956",
-  // dialect: "postgres",
+  HOST: "ec2-3-230-106-126.compute-1.amazonaws.com",
+  DB: "d5hdlrtrpck956",
+  USER: "qrhhuvveqvggwl",
+  PASSWORD: "883fb7ebc1603c4e55e172b91fb40a4087f72255a7be7ed1564b176f36e58a24",
+  dialect: "postgres",
   
   pool: {
     max: 5,
