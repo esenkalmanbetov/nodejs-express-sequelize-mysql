b13196e4b1c427ecccee6736b1ea37e741756a2b
Esen <esenkalmanbetov@gmail.com>
2020-02-29 20:07:40 +0600

    a

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -4,12 +4,11 @@ module.exports = {
   // USER: "root",
   // PASSWORD: "password",
   // dialect: "mysql",
-
-  HOST: "ec2-184-72-235-159.compute-1.amazonaws.com",
-  DB: "da07p9af7715lr",
-  USER: "zmtbzshhblgyzj",
-  PASSWORD: "939212a0f67de60a52dd1a75133639cacc284255a5fba93b2c1a4ffe7ca25a53",
-  dialect: "postgres",
+  HOST: process.env.HOST || "localhost",//"http://ec2-184-72-235-159.compute-1.amazonaws.com",
+  DB: process.env.DB ||  "testdb",//"da07p9af7715lr",
+  USER:process.env.USER || "root",// "zmtbzshhblgyzj",
+  PASSWORD: process.env.PASSWORD || "password",//"939212a0f67de60a52dd1a75133639cacc284255a5fba93b2c1a4ffe7ca25a53",
+  dialect: process.env.dialect || "mysql",//"postgres",
 
   pool: {
     max: 5,
--- server.js
+++ server.js
@@ -33,4 +33,5 @@ require("./app/routes/tutorial.routes")(app);
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}/`);
+  
 });
