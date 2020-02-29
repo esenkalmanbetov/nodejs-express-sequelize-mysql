56fa47c093d17f98292d2a3504ef9ca29fd12043
Esen <esenkalmanbetov@gmail.com>
2020-02-29 18:43:52 +0600

    added remotedb

--- server.js
+++ server.js
@@ -32,5 +32,5 @@ require("./app/routes/tutorial.routes")(app);
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+  console.log(`Server is running on port http://localhost:${PORT}/`);
 });
