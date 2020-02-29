519ff609036386d88c9e8e3622069b61095ea6ff
Esen <esenkalmanbetov@gmail.com>
2020-02-29 19:50:41 +0600

    fadf

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,10 +1,16 @@
 module.exports = {
-  HOST: "ec2-3-230-106-126.compute-1.amazonaws.com",
-  DB: "d5hdlrtrpck956",
-  USER: "qrhhuvveqvggwl",
-  PASSWORD: "883fb7ebc1603c4e55e172b91fb40a4087f72255a7be7ed1564b176f36e58a24",
-  dialect: "mysql",
-  
+  // HOST: "localhost",
+  // DB: "testdb",
+  // USER: "root",
+  // PASSWORD: "password",
+  // dialect: "mysql",
+
+  HOST: "ec2-184-72-235-159.compute-1.amazonaws.com",
+  DB: "da07p9af7715lr",
+  USER: "zmtbzshhblgyzj",
+  PASSWORD: "939212a0f67de60a52dd1a75133639cacc284255a5fba93b2c1a4ffe7ca25a53",
+  dialect: "postgres",
+
   pool: {
     max: 5,
     min: 0,
