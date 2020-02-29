fce725afee276c108e0169f0bdd1608b120e2dcc
Esen <esenkalmanbetov@gmail.com>
2020-02-29 19:04:05 +0600

    fasd

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,15 +1,9 @@
 module.exports = {
-  // HOST: "localhost",
-  // USER: "root",
-  // PASSWORD: "password",
-  // DB: "testdb",
-  // dialect: "mysql",
-  
-  // HOST: "ec2-3-230-106-126.compute-1.amazonaws.com",
-  // DB: "d5hdlrtrpck956",
-  // USER: "qrhhuvveqvggwl",
-  // PASSWORD: "883fb7ebc1603c4e55e172b91fb40a4087f72255a7be7ed1564b176f36e58a24",
-  // dialect: "postgres",
+  HOST: "localhost",
+  USER: "root",
+  PASSWORD: "password",
+  DB: "testdb",
+  dialect: "mysql",
   
   pool: {
     max: 5,
--- app/models/index.js
+++ app/models/index.js
@@ -1,20 +1,20 @@
 // const dbConfig = require("../config/db.config.js");
 
 const Sequelize = require("sequelize");
-// const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-//   host: dbConfig.HOST,
-//   dialect: dbConfig.dialect,
-//   operatorsAliases: false,
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+  host: dbConfig.HOST,
+  dialect: dbConfig.dialect,
+  operatorsAliases: false,
 
-//   pool: {
-//     max: dbConfig.pool.max,
-//     min: dbConfig.pool.min,
-//     acquire: dbConfig.pool.acquire,
-//     idle: dbConfig.pool.idle
-//   }
-// });
+  pool: {
+    max: dbConfig.pool.max,
+    min: dbConfig.pool.min,
+    acquire: dbConfig.pool.acquire,
+    idle: dbConfig.pool.idle
+  }
+});
 
-const sequelize = new Sequelize('postgres://qrhhuvveqvggwl:883fb7ebc1603c4e55e172b91fb40a4087f72255a7be7ed1564b176f36e58a24@ec2-3-230-106-126.compute-1.amazonaws.com:5432/d5hdlrtrpck956');
+// const sequelize = new Sequelize('postgres://qrhhuvveqvggwl:883fb7ebc1603c4e55e172b91fb40a4087f72255a7be7ed1564b176f36e58a24@ec2-3-230-106-126.compute-1.amazonaws.com:5432/d5hdlrtrpck956');
 
 const db = {};
 
