45a82585a1cbe6be2a077f2f07ca0b3e19252467
Esen <esenkalmanbetov@gmail.com>
2020-02-29 20:41:03 +0600

    fa

--- server.js
+++ server.js
@@ -18,13 +18,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const db = require("./app/models");
 
-// db.sequelize.sync({ force: true }).then(() => {
-//   console.log("Drop and re-sync db.");
-// });
+db.sequelize.sync()
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+  res.json({ message: "Welcome to esen application." });
 });
 
 require("./app/routes/tutorial.routes")(app);
