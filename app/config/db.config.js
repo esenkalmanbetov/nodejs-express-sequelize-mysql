febd1b4f5b926a9bb66c254be90a70891161de00
Esen <esenkalmanbetov@gmail.com>
2020-02-29 18:48:46 +0600

    modified

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,15 +1,15 @@
 module.exports = {
-  // HOST: "localhost",
-  // USER: "root",
-  // PASSWORD: "password",
-  // DB: "testdb",
-  // dialect: "mysql",
+  HOST: "localhost",
+  USER: "root",
+  PASSWORD: "password",
+  DB: "testdb",
+  dialect: "mysql",
   
-  HOST: "ec2-3-230-106-126.compute-1.amazonaws.com",
-  USER: "qrhhuvveqvggwl",
-  PASSWORD: "883fb7ebc1603c4e55e172b91fb40a4087f72255a7be7ed1564b176f36e58a24",
-  DB: "d5hdlrtrpck956",
-  dialect: "postgres",
+  // HOST: "ec2-3-230-106-126.compute-1.amazonaws.com",
+  // USER: "qrhhuvveqvggwl",
+  // PASSWORD: "883fb7ebc1603c4e55e172b91fb40a4087f72255a7be7ed1564b176f36e58a24",
+  // DB: "d5hdlrtrpck956",
+  // dialect: "postgres",
   
   pool: {
     max: 5,
